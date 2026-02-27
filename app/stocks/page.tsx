caf1b01e6b2bde5856b57d6c5cf2f943b3078e65
Jack LeBlanc <leblanc.jack00@gmail.com>
2026-02-27 14:10:14 -0500

    fix useRef import

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { createClient } from '../lib/supabase'
 
 const font = "'JetBrains Mono', 'Fira Code', monospace"
